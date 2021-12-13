c25db5dc383ff734ebd42aca98d8045ab4f99a33
nikalakhno <96026488+nikalakhno@users.noreply.github.com>
2021-12-13 16:42:01 +0200

    Add files via upload

--- mtzkit 2/Program.cs
+++ mtzkit 2/Program.cs
@@ -6,8 +6,20 @@ namespace mtzkit_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("Введіть кількість цілих чисел");
+            int num = int.Parse(Console.ReadLine());
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            for (int i = 1; i <= num; i = i + 1)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a < min)
+                    min = a;
+                if (a > max)
+                    max = a;
+            }
+            Console.WriteLine(max + min);
+
         }
     }
 }
